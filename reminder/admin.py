1f906753184e4dc3d54a88c085b9986152c33719
Uday Kiran <uday1kiran@gmail.com>
2020-09-21 17:56:25 +0530

    Added Reminder model ad registered with admin page with readonly fields visible

--- reminder/admin.py
+++ reminder/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Reminder
 # Register your models here.
+
+#to show read only fields like datecreated
+class ReminderAdmin(admin.ModelAdmin):
+    readonly_fields = ('created',)
+
+#admin.site.register(Reminder)
+admin.site.register(Reminder,ReminderAdmin)
